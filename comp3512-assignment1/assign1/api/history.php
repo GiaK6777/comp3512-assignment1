d1927356d222e48b4b0d5e57ac01aef8130f7b7d
GiaKN@5777 <gnguy930@mtroyal.ca>
2025-10-19 10:08:04 -0600

    Update history.php

--- comp3512-assignment1/assign1/api/history.php
+++ comp3512-assignment1/assign1/api/history.php
@@ -1,8 +1,48 @@
-require_once 'config.inc.php';
+<?php
+//../
+require_once '../config.inc.php';  
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
 
 try {
-    $pdo = new PDO(DBCONNSTRING, DBUSER, DBPASS);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (Exception $e) {
-    die($e->getMessage());
+   // $pdo = new PDO(DBCONNSTRING, DBUSER, DBPASS);
+   // $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // get sympol element from database to check
+    if (!isset($_GET['symbol'])) {
+        echo json_encode(["error" => "Missing required parameter (symbol)"]);
+        exit;
+    }
+
+    $symbol = $_GET['symbol'];
+
+   
+    if (isset($_GET['startDate']) && isset($_GET['endDate'])) {
+        $sql = "SELECT date, volume, open, close, high, low
+                FROM history
+                WHERE symbol = :symbol
+                  AND date BETWEEN :startDate AND :endDate
+                ORDER BY date ASC";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':symbol', $symbol);
+        $stmt->bindValue(':startDate', $_GET['startDate']);
+        $stmt->bindValue(':endDate', $_GET['endDate']);
+    } else {
+        
+        $sql = "SELECT date, volume, open, close, high, low
+                FROM history
+                WHERE symbol = :symbol
+                ORDER BY date ASC";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':symbol', $symbol);
+    }
+
+    $stmt->execute();
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    echo json_encode($rows, JSON_PRETTY_PRINT | JSON_NUMERIC_CHECK);
+
+} catch (PDOException $e) {
+    echo json_encode(["error" => $e->getMessage()]);
 }
+?>
