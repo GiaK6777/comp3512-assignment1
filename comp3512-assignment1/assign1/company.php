886c572ef85ba8adef2e08a054e25fe10348dc82
asofo455 <46725036+asofo455@users.noreply.github.com>
2025-10-19 13:07:52 -0600

    Updated some of the company page

--- comp3512-assignment1/assign1/company.php
+++ comp3512-assignment1/assign1/company.php
@@ -1,14 +1,10 @@
-<?php
-require_once 'config.inc.php';
 
-try {
-    $pdo = new PDO(DBCONNSTRING, DBUSER, DBPASS);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (Exception $e) {
-    die($e->getMessage());
-}
+<?php
+require_once './includes/config.inc.php';
+
 ?>
-    
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -19,17 +15,37 @@ try {
 </head>
 <body>
 <main class="container">
-
-    <div class="box"> 
       <h1>Company Page</h1>
-      <div class="">
-         
-      </div>
-    </div>
+<table class="">
+<thead>    
+<tr>
+            <th> Name</th>
+                <th> Symbol</th>
+                    <th> Sector</th>
+                        <th>subindustry</th>
+                            <th>address</th>
+                                 <th>exchange</th>
+                                    <th>website</th>
+                                        <th>description</th>
+                                            <th>financials</th>
+        </tr>
+</thead>
+        <tbody>
+        </tbody>
 
+</table>
+<div class="box">
+    <h2>History (3M)</h2>
+    <div>
+        <table>
+
+</div>
+<h3>History High</h3>
+<h3>History Low</h3>
+<h3>Total Volume</h3>
+<h3>Average Volume</h3>
 </main>   
 </body>
 <footer>
 </footer>
-
 </html>
